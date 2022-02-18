0721c8e71aa6fc33512e5ecf0830661e57c61a6b
Raphael THIBAUT <raphael.dev@protonmail.com>
2022-02-18 12:00:11 +0100

    test helloWorld smartContract

---
+++ test/helloWorld.test.ts
@@ -0,0 +1,13 @@
+import _ from "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("Test if the contract is well declared", function() {
+    it("should say hi", async function() {
+        const HelloWorld = await ethers.getContractFactory("helloWorldOui");
+        const hello = await HelloWorld.deploy();
+        await hello.deployed();
+
+        expect(await hello.hello()).to.equal("Hello World !")
+    })
+})
